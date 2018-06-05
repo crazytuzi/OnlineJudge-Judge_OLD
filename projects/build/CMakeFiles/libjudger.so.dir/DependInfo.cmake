fdcde7e84778bb33e030b549eaeeb0a7fef3f6a7
crazytuzi <957824770@qq.com>
2018-06-05 21:49:23 +0800

    Initial commit

---
+++ projects/build/CMakeFiles/libjudger.so.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/olily/桌面/Judger-newnew/src/argtable3.c" "/home/olily/桌面/Judger-newnew/build/CMakeFiles/libjudger.so.dir/src/argtable3.o"
+  "/home/olily/桌面/Judger-newnew/src/child.c" "/home/olily/桌面/Judger-newnew/build/CMakeFiles/libjudger.so.dir/src/child.o"
+  "/home/olily/桌面/Judger-newnew/src/killer.c" "/home/olily/桌面/Judger-newnew/build/CMakeFiles/libjudger.so.dir/src/killer.o"
+  "/home/olily/桌面/Judger-newnew/src/logger.c" "/home/olily/桌面/Judger-newnew/build/CMakeFiles/libjudger.so.dir/src/logger.o"
+  "/home/olily/桌面/Judger-newnew/src/main.c" "/home/olily/桌面/Judger-newnew/build/CMakeFiles/libjudger.so.dir/src/main.o"
+  "/home/olily/桌面/Judger-newnew/src/rules/c_cpp.c" "/home/olily/桌面/Judger-newnew/build/CMakeFiles/libjudger.so.dir/src/rules/c_cpp.o"
+  "/home/olily/桌面/Judger-newnew/src/rules/general.c" "/home/olily/桌面/Judger-newnew/build/CMakeFiles/libjudger.so.dir/src/rules/general.o"
+  "/home/olily/桌面/Judger-newnew/src/runner.c" "/home/olily/桌面/Judger-newnew/build/CMakeFiles/libjudger.so.dir/src/runner.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
